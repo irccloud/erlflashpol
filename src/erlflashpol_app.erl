d6db3ba8b8a0b006c233f0dd5600e58bdab3ba33
Richard Jones <rj@metabrew.com>
2011-07-12 17:41:04 +0100

    add a warning if no app env set, add info msg declaring port and policy on startup

--- src/erlflashpol_app.erl
+++ src/erlflashpol_app.erl
@@ -9,12 +9,15 @@ start() -> application:start(erlflashpol).
 start(_StartType, _StartArgs) ->
     Config =
         case application:get_all_env(erlflashpol) of
-            undefined -> [];
+            undefined -> 
+                error_logger:warning_msg("erlflashpol has no application env set, using defaults with *LIBERAL POLICY*\n"),
+                [];
             L -> L
         end,
     ListenIp = proplists:get_value(listen_ip, Config, any),
     ListenPort = proplists:get_value(listen_port, Config, 8843),
     PolicyFile = proplists:get_value(policy_file, Config, liberal),
+    error_logger:info_msg("Starting flash policy server on ~p:~p with policy ~p\n", [ListenIp, ListenPort, PolicyFile]),
     erlflashpol_sup:start_link(ListenIp, ListenPort, PolicyFile).
 
 stop(_State) -> ok.
